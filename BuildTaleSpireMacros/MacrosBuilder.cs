0113df3f6a0452ac5bc60cdb12d7978561a19407
Lord Ashes <webtable@gmail.com>
2021-09-04 13:39:44 -0400

    Version 1.0.0

---
+++ BuildTaleSpireMacros/MacrosBuilder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordAshes
+{
+    class MacroBuilder
+    {
+        static void Main(string[] args)
+        {
+            BuildMacroPluginSourceCode();
+            BuildMacrosPlugin();
+        }
+
+        static void BuildMacroPluginSourceCode()
+        {
+            string template = System.IO.File.ReadAllText("macro_template.pcs");
+            string check = "";
+            string execute = "";
+            int count = 1;
+            string[] macroFiles = System.IO.Directory.EnumerateFiles("./macros", "*.macroScript").ToArray();
+            foreach (string macroFile in macroFiles)
+            {
+                string[] contents = System.IO.File.ReadAllLines(macroFile);
+                string title = "";
+                string button = "";
+                int mode = 0;
+                string checkCode = "";
+                string executeCode = "";
+                foreach (string line in contents)
+                {
+                    if (line.ToUpper().StartsWith("MACRO:")) { title = line.Substring(6); }
+                    if (line.ToUpper().StartsWith("BUTTON:")) { button = line.Substring(7); }
+                    if (line.ToUpper().StartsWith("CHECK:")) { mode = 1; }
+                    else if (line.ToUpper().StartsWith("EXECUTE:")) { mode = 2; }
+                    else if (mode == 1) { checkCode = checkCode + "\t\t\t" + line + "\r\n"; }
+                    else if (mode == 2) { executeCode = executeCode + "\t\t\t" + line + "\r\n"; }
+                }
+
+                while (checkCode.StartsWith("\t\t\t\r\n")) { checkCode = checkCode.Substring(5); }
+                while (executeCode.StartsWith("\t\t\t\r\n")) { executeCode = executeCode.Substring(5); }
+
+                check = check + "\t\t\t\t//\r\n";
+                check = check + "\t\t\t\t// Macro: " + title + "\r\n";
+                check = check + "\t\t\t\t//\r\n";
+                check = check + "\t\t\t\tif(Macro_" + count.ToString("d2") + "_Available())\r\n";
+                check = check + "\t\t\t\t{\r\n";
+                check = check + "\t\t\t\t\tif(GUI.Button(new Rect(1920-macroMenuSlide, offset, 160, 40), \"" + button + "\")) { Macro_" + count.ToString("d2") + "_Execute(); }\r\n";
+                check = check + "\t\t\t\t\toffset = offset + 50;\r\n";
+                check = check + "\t\t\t\t}\r\n";
+
+                execute = execute + "\t\tpublic bool Macro_" + count.ToString("d2") + "_Available()\r\n\t\t{\r\n" + checkCode + "\r\n\t\t}\r\n\r\n";
+                execute = execute + "\t\tpublic void Macro_" + count.ToString("d2") + "_Execute()\r\n\t\t{\r\n" + executeCode + "\r\n\t\t}\r\n";
+
+                count++;
+            }
+            System.IO.File.WriteAllText("./MacrosPlugin/MacrosPlugin.cs", template.Replace("{Check}", check).Replace("{Execute}", execute));
+        }
+
+        public static void BuildMacrosPlugin()
+        {
+            string[] config = System.IO.File.ReadAllLines("MacrosBuilder.config");
+            Process build = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    WorkingDirectory = "./MacrosPlugin",
+                    FileName = config[0],
+                    Arguments = "MacrosPlugin.csproj",
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                }
+            };
+            build.Start();
+            StreamReader reader = build.StandardOutput;
+            string output = reader.ReadToEnd();
+            Console.WriteLine(output);
+            build.WaitForExit();
+            if(System.IO.File.Exists("./MacrosPlugin/bin/Debug/MacrosPlugin.dll"))
+            {
+                if (System.IO.File.Exists(config[1] + "/MacrosPlugin.dll")) { System.IO.File.Delete(config[1] + "/MacrosPlugin.dll"); }
+                System.IO.File.Copy("./MacrosPlugin/bin/Debug/MacrosPlugin.dll", config[1] + "/MacrosPlugin.dll");
+            }
+        }
+    }
+}
---
+++ BuildTaleSpireMacros/MacrosPlugin/MacrosPlugin.cs
@@ -0,0 +1,212 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+using UnityEngine;
+
+namespace LordAshes
+{
+	[BepInPlugin(Guid, Name, Version)]
+	[BepInDependency(LordAshes.FileAccessPlugin.Guid)]
+	[BepInDependency(LordAshes.StatMessaging.Guid)]
+	[BepInDependency(RadialUI.RadialUIPlugin.Guid)]
+	public partial class MacrosPlugin : BaseUnityPlugin
+	{
+		// Plugin info
+		public const string Name = "Macros Plug-In";
+		public const string Guid = "org.lordashes.plugins.macrosplugin";
+		public const string Version = "1.0.0.0";
+
+		public bool macroMenu = false;
+		public int macroMenuSlide = 0;
+
+		public System.Random random = new System.Random();
+
+		/// <summary>
+		/// Function for initializing plugin
+		/// This function is called once by TaleSpire
+		/// </summary>
+		void Awake()
+		{
+			UnityEngine.Debug.Log("Macros Plugin: Active.");
+
+			// Utility.PostOnMainPage(this.GetType());
+		}
+
+		/// <summary>
+		/// Function for determining if view mode has been toggled and, if so, activating or deactivating Character View mode.
+		/// This function is called periodically by TaleSpire.
+		/// </summary>
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Backslash))
+			{
+				Debug.Log("Macro Menu Open = " + macroMenu);
+				macroMenu = !macroMenu;
+			}
+			if ((macroMenu) && (macroMenuSlide < 160)) { macroMenuSlide = macroMenuSlide + 4; }
+			if ((!macroMenu) && (macroMenuSlide > 0)) { macroMenuSlide = macroMenuSlide - 4; }
+		}
+
+		void OnGUI()
+		{
+			if (macroMenuSlide > 0)
+			{
+				int offset = 50;
+				//
+				// Macro: Attack1
+				//
+				if (Macro_01_Available())
+				{
+					if (GUI.Button(new Rect(1920 - macroMenuSlide, offset, 160, 40), "Short Sword (+3)")) { Macro_01_Execute(); }
+					offset = offset + 50;
+				}
+				//
+				// Macro: Attack2
+				//
+				if (Macro_02_Available())
+				{
+					if (GUI.Button(new Rect(1920 - macroMenuSlide, offset, 160, 40), "Dagger (+3)")) { Macro_02_Execute(); }
+					offset = offset + 50;
+				}
+				//
+				// Macro: Attack3
+				//
+				if (Macro_03_Available())
+				{
+					if (GUI.Button(new Rect(1920 - macroMenuSlide, offset, 160, 40), "Toss (+5)")) { Macro_03_Execute(); }
+					offset = offset + 50;
+				}
+
+			}
+		}
+
+		/* ----------------------------------------- Macro Functions ----------------------------------------- */
+
+		public bool Macro_01_Available()
+		{
+			return (SelectedCreature() != null);
+
+
+		}
+
+		public void Macro_01_Execute()
+		{
+			Creature attacker = SelectedCreature();
+			Creature target = RadialCreature();
+
+			if (attacker != null && target != null)
+			{
+				int roll = Random(1, 20) + 3;
+				if (roll >= target.Stat0.Value)
+				{
+					attacker.Speak("Attack" + roll + "\r\nHit!");
+					int dmg = Random(1, 6) + 3;
+					target.Speak("Sliced for\r\n" + dmg + " HP");
+				}
+				else
+				{
+					attacker.Speak("Attack " + roll + "\r\nMiss!");
+					target.Speak("Missed me!");
+				}
+			}
+
+
+		}
+		public bool Macro_02_Available()
+		{
+			return (SelectedCreature() != null);
+
+
+		}
+
+		public void Macro_02_Execute()
+		{
+			Creature attacker = SelectedCreature();
+			Creature target = RadialCreature();
+
+			if (attacker != null && target != null)
+			{
+				int roll = Random(1, 20) + 3;
+				if (roll >= target.Stat0.Value)
+				{
+					attacker.Speak("Attack" + roll + "\r\nHit!");
+					int dmg = Random(1, 4) + 3;
+					target.Speak("Stabbed for\r\n" + dmg + " HP");
+				}
+				else
+				{
+					attacker.Speak("Attack " + roll + "\r\nMiss!");
+					target.Speak("Missed me!");
+				}
+			}
+
+
+		}
+		public bool Macro_03_Available()
+		{
+			if (SelectedCreature() == null) { return false; }
+			if (!SelectedCreature().Name.Contains("Monkey")) { return false; }
+			return true;
+
+
+		}
+
+		public void Macro_03_Execute()
+		{
+			Creature attacker = SelectedCreature();
+			Creature target = RadialCreature();
+
+			if (attacker != null && target != null)
+			{
+				int roll = Random(1, 20) + 5;
+				if (roll >= target.Stat0.Value)
+				{
+					attacker.Speak("Attack" + roll + "\r\nHit!");
+					int dmg = Random(1, 6) + Random(1, 6) + 3;
+					target.Speak("Stabbed for\r\n" + dmg + " HP");
+				}
+				else
+				{
+					attacker.Speak("Attack " + roll + "\r\nMiss!");
+					target.Speak("Missed me!");
+				}
+			}
+
+
+		}
+
+
+		/* ----------------------------------------- Helper Functions ----------------------------------------- */
+
+		public CreatureBoardAsset SelectedAsset()
+		{
+			CreatureBoardAsset asset;
+			CreaturePresenter.TryGetAsset(LocalClient.SelectedCreatureId, out asset);
+			return asset;
+		}
+
+		public CreatureBoardAsset RadialAsset()
+		{
+			CreatureBoardAsset asset;
+			CreaturePresenter.TryGetAsset(new CreatureGuid(RadialUI.RadialUIPlugin.GetLastRadialTargetCreature()), out asset);
+			return asset;
+		}
+
+		public Creature SelectedCreature()
+		{
+			CreatureBoardAsset asset = SelectedAsset();
+			return (asset != null) ? asset.Creature : null;
+		}
+
+		public Creature RadialCreature()
+		{
+			CreatureBoardAsset asset = RadialAsset();
+			return (asset != null) ? asset.Creature : null;
+		}
+
+		public int Random(int min, int max)
+		{
+			return random.Next(min, max);
+		}
+	}
+}
